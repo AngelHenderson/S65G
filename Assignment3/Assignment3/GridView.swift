2d097ceb01e3703c4690072193b371331e4f7bc0
AngelHenderson <angel.henderson@me.com>
2016-07-09 20:05:03 -0400

    Grid View Made

--- Assignment3/Assignment3/GridView.swift
+++ Assignment3/Assignment3/GridView.swift
@@ -10,8 +10,8 @@ import UIKit
 
 @IBDesignable class GridView: UIView {
 
-    @IBInspectable var rows:Int = 20
-    @IBInspectable var cols:Int = 20
+    //@IBInspectable var rows:Int = 20
+    //@IBInspectable var cols:Int = 20
     @IBInspectable var livingColor = UIColor.greenColor()
     @IBInspectable var emptyColor = UIColor.whiteColor()
     @IBInspectable var bornColor = UIColor.blueColor()
@@ -19,5 +19,30 @@ import UIKit
     @IBInspectable var gridColor = UIColor.grayColor()
     @IBInspectable var gridWidth = CGFloat(10)
 
+    var grid = [[CellState]]()
 
+    @IBInspectable var rows : Int = 20{
+        
+        //First this
+        willSet {
+            //print("Old value is \(rows), new value is \(newValue)")
+        }
+        
+        //Finaly this
+        didSet {
+            grid = Array (count: rows, repeatedValue: Array(count: cols, repeatedValue: .Empty))
+            //print("Old value is \(oldValue), new value is \(grid)")
+        }
+    }
+    
+    @IBInspectable var cols : Int = 20 {
+        willSet {
+            //print("Old value is \(rows), new value is \(newValue)")
+        }        
+        didSet {
+            grid = Array (count: rows, repeatedValue: Array(count: cols, repeatedValue: .Empty))
+            //print("Old value is \(oldValue), new value is \(grid)")
+        }
+    }
+    
 }
