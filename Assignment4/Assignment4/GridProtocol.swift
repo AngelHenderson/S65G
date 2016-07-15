7ecfdf6b4617036159fd59c8556eb525d1bdeee8
AngelHenderson <angel.henderson@me.com>
2016-07-15 15:11:45 -0400

    Problem 3 working on GridProtocol

--- Assignment4/Assignment4/GridProtocol.swift
+++ Assignment4/Assignment4/GridProtocol.swift
@@ -53,3 +53,69 @@ protocol GridProtocol {
 protocol EngineDelegate {
     func engineDidUpdate(withGrid: GridProtocol)
 }
+
+protocol EngineProtocol {
+    var delegate: EngineDelegate? { get set }
+    var grid: GridProtocol { get }
+    var refreshRate: Double { get set }
+    var refreshInterval: NSTimeInterval { get set }
+    var rows: UInt { get set }
+    var cols: UInt { get set }
+    
+    init(rows: UInt, cols: UInt)
+    func step() -> GridProtocol
+}
+
+
+class Grid : GridProtocol {
+    
+    private var cells : [CellState] =  Array<CellState>(count: 100, repeatedValue: .Empty)
+
+
+    var rows: UInt = 0
+    var cols: UInt = 0
+    
+    required init(rows: UInt,cols: UInt) {
+        self.rows  = rows
+        self.cols = cols
+        
+        cells = Array<CellState>(count: Int(self.rows), repeatedValue: .Empty)
+        //cells = Array (count: Int(self.rows), repeatedValue: Array(count: Int(self.cols), repeatedValue: .Empty))
+
+    }
+    
+    private var timer:NSTimer?
+    
+    var refreshInterval: NSTimeInterval = 0 {
+        didSet {
+            if refreshInterval != 0 {
+                if let timer = timer { timer.invalidate() }
+                let sel = #selector(Grid.timerDidFire(_:))
+                timer = NSTimer.scheduledTimerWithTimeInterval(refreshInterval,
+                                                               target: self,
+                                                               selector: sel,
+                                                               userInfo: ["name": "fred"],
+                                                               repeats: true)
+            }
+            else if let timer = timer {
+                timer.invalidate()
+                self.timer = nil
+            }
+        }
+    }
+
+    @objc func timerDidFire(timer:NSTimer) {
+        self.rows += 1
+        let center = NSNotificationCenter.defaultCenter()
+        let n = NSNotification(name: "ExampleNotification",
+                               object: nil,
+                               userInfo: ["name": "fred"])
+        center.postNotification(n)
+        print ("\(timer.userInfo?["name"] ?? "not fred")")
+    }
+    
+    var delegate: ExampleDelegateProtocol?
+    func step() -> [[Bool]] {
+        return [[false]]
+    }
+}
