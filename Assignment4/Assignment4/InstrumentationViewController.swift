8b37d3975f99eec517a52ac4a5fa7cee3bedb56c
AngelHenderson <angel.henderson@me.com>
2016-07-18 09:22:37 -0400

    Initial View for Instruments

--- Assignment4/Assignment4/InstrumentationViewController.swift
+++ Assignment4/Assignment4/InstrumentationViewController.swift
@@ -9,9 +9,17 @@
 import UIKit
 
 class InstrumentationViewController: UIViewController {
+    var gameEngine: StandardEngine!
 
+    @IBOutlet weak var timeIntervalTextField: UITextField!
+    @IBOutlet weak var rowStepper: UIStepper!
+    @IBOutlet weak var rowCountTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        gameEngine = StandardEngine.sharedInstance
+
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,6 +27,22 @@ class InstrumentationViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    @IBAction func increment(sender: AnyObject) {
+        gameEngine.rows += 10
+    }
+    
+    @IBAction func updateTimeInterval(sender: UITextField) {
+        if let text = sender.text,
+            interval = Double(text)  {
+            gameEngine.refreshInterval = interval
+        }
+        else  {
+            gameEngine.refreshInterval = 0
+        }
+    }
+    
 
 
 }
