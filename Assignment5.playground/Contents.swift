e8276c659495b042ebccddcfc2c316046dedab31
AngelHenderson <angel.henderson@me.com>
2016-07-24 15:02:48 -0400

    Cleanup

--- Assignment5.playground/Contents.swift
+++ Assignment5.playground/Contents.swift
@@ -2,9 +2,7 @@
 
 import UIKit
 
-let daysInMonth:[Int] = [
-   0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31
-]
+let daysInMonth:[Int] = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
 func isLeap(year:Int) -> Bool {
     let leapYear = (year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)
@@ -12,21 +10,10 @@ func isLeap(year:Int) -> Bool {
 }
 
 func julianDate(year: Int, month: Int, day: Int) -> Int {
-    let yearDuration: Int = Array(1900...year).filter{$0 % 1900 != 0}.reduce(0, combine:{$0 + (isLeap($1) != true ? 365 : 366)})
+    let yearDuration: Int = Array(1900...year).filter{$0 % year != 0}.reduce(0, combine:{$0 + (isLeap($1) != true ? 365 : 366)})
     let monthDuration: Int = Array(1...month).filter{$0 % month != 0}.reduce(0, combine:{$0 + daysInMonth[$1]})
-    //return yearDuration + monthDuration + day + (isLeap(year) == true && month > 2 ? 1 : 0)
-    return yearDuration + monthDuration + day
-}
-
-func julianDate2(year: Int, month: Int, day: Int) -> Int {
-
-//    let originalDate = (365*1900 + 1900/4 - 1900/100 + 1900/400 + (153*1 + 2)/5 + 1)
-//    let julianDateDifference = (365*year + year/4 - year/100 + year/400 + (153*month + 2)/5 + day) - originalDate
-    //let leapYearCheck = isLeap(year) ? 1 : 0
-
-    let originalDate = (365*1900 + 1900/4 - 1900/100 + 1900/400 + (153*1 - 457)/5 + 1 - 306)
-    let julianDateDifference = (365*year + year/4 - year/100 + year/400 + (153*month - 457)/5 + day - 306) - originalDate
-    return julianDateDifference
+    let februaryCheck: Int = (isLeap(year) == true && month > 2 ? 1 : 0)
+    return yearDuration + monthDuration + day + februaryCheck
 }
 
 julianDate(1960, month:  9, day: 28)
@@ -36,45 +23,6 @@ julianDate(1901, month: 1, day: 1)
 julianDate(1901, month: 1, day: 1) - julianDate(1900, month: 1, day: 1)
 julianDate(2001, month: 1, day: 1) - julianDate(2000, month: 1, day: 1)
 
-isLeap(2000)
+isLeap(1960)
 isLeap(1900)
 isLeap(2000)
-
-
-let leapYearCheck = isLeap(2001) ? 1 : 0
-
-//myBool = str1 ? true : false
-//let leapYearCheck = isLeap(year) ? 1 : 0
-//func reduce<U>(initial: U, combine: (U, T) -> U) -> U
-
-let sum:Int = [1, 2, 3, 4, 5].reduce(0, combine: { $1 + $0 + 1})
-
-let evenSum:Int = Array(1...3).filter { (number) in number % 2 == 0 }.reduce(0) { (total, number) in total + number }
-
-let smallSum:Int = Array(1...1).reduce(0, combine: { $1 + $0})
-
-evenSum
-sum
-smallSum
-
-
-
-julianDate2(1908, month: 3, day: 4)
-//daysInMonth[2]
-
-(2 % 3 != 0)
-
-julianDate(1959, month:  12, day: 31)
-julianDate(1960, month:  1, day: 1)
-
-julianDate(1955, month:  12, day: 31)
-julianDate(1956, month:  1, day: 1)
-
-julianDate(1900, month:  12, day: 31)
-julianDate(1901, month:  1, day: 1)
-
-julianDate(1903, month:  12, day: 31)
-julianDate(1904, month:  1, day: 1)
-//
-//(1960 % 1900 != 0)
-//(1959 % 1900 != 0)
