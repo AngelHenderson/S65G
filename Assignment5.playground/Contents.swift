a09a1735b50e47bb65589c2f23dfcccbcfb98a18
AngelHenderson <angel.henderson@me.com>
2016-07-25 15:31:55 -0400

    Cleanup

--- Assignment5.playground/Contents.swift
+++ Assignment5.playground/Contents.swift
@@ -24,3 +24,12 @@ julianDate(2001, month: 1, day: 1) - julianDate(2000, month: 1, day: 1)
 isLeap(1960)
 isLeap(1900)
 isLeap(2000)
+
+
+
+
+//Class Project Bonus Proposal:
+
+//My proposal in this project is to add changes to the model to explore different rule options, specifically mutation. This would be one of two new Grids that I would provide for the Final Project. It would support random mutation for different cells once every 5 steps.
+
+//The second Grid would be a change to the UI to create an interesting theme and entertaining user experience. The theme would be a Pac-Man theme that would not only change the color design of the Grid but would include a small Pac-Man moving through the Grid every step and any cell it crossed would go through a function that could turn a current cell state into a random cell state.
